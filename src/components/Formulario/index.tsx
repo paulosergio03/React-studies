270ed7feca1da53307c7d0fcfc8d41758510563d
Paulo Sergio <paulosergioadm9@gmail.com>
2022-04-08 08:36:15 -0300

    Alteração

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,40 +1,40 @@
 import React from 'react';
 import Botao from '../Botao';
-
+import './style.scss';
 class Formulario extends React.Component {
   render() {
-      return (
-          <form>
-            <div>
-              <label htmlFor="tarefa">
-                 Adicione um novo estudo
-               </label>
-               <input
-                  type="text"
-                  name="tarefa"
-                  id="tarefa"
-                  placeholder="O que você quer estudar"
-                  required
-                />
-                            </div>
-            <div>
-              <label htmlFor="tempo">`
-                Tempo
-              </label>
-              <input
-                type="time"
-                step="1"
-                name="tempo"
-                id="tempo"
-                min="00:00:00"
-                max="01:30:00"
-                required
-              />
-            </div>
-          <Botao />
-        </form>
-      )
-    }
+    return (
+      <form className="novaTarefa">
+        <div className="inputContainer">
+          <label htmlFor="tarefa">
+            Adicione um novo estudo
+          </label>
+          <input
+            type="text"
+            name="tarefa"
+            id="tarefa"
+            placeholder="O que você quer estudar"
+            required
+          />
+        </div>
+        <div className="inputContainer">
+          <label htmlFor="tempo">`
+            Tempo
+          </label>
+          <input
+            type="time"
+            step="1"
+            name="tempo"
+            id="tempo"
+            min="00:00:00"
+            max="01:30:00"
+            required
+          />
+        </div>
+        <Botao />
+      </form>
+    )
   }
+}
 
-  export default Formulario;
+export default Formulario;
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './style.scss';
 
 function Lista() {
     const tarefa = [{
@@ -14,13 +15,13 @@ function Lista() {
     }]
 
     return (
-        <aside>
+        <aside className="listaTarefas">
             <h2> Estudos do dia </h2>
 
             <ul>
                 {tarefa.map((item, index) => (
 
-                    <li key={index}>
+                    <li key={index} className="item">
                         <h3> {item.tarefa} </h3>
                         <span> {item.tempo} </span>
                     </li>
